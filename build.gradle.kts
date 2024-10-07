f74a04d0b15d57aa27e126571eb8a98733e7540a
E85Addict <77761710+E85Addict@users.noreply.github.com>
2024-10-07 01:00:13 -0700

    feat(YouTube): Support versions 19.25 and 19.34

--- build.gradle.kts
+++ build.gradle.kts
@@ -108,7 +108,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/revanced/revanced-patches")
+            url = uri("https://maven.pkg.github.com/E85Addicts/revanced-patches")
             credentials {
                 username = System.getenv("GITHUB_ACTOR")
                 password = System.getenv("GITHUB_TOKEN")
